997bdf2d6ff27fd2d2949fb401f89d4f9a2a5b2e
Nishtha Kaigaonkar <nishtha@Nishthas-MacBook-Air.local>
2023-08-10 17:51:19 +0530

    adding files

---
+++ src/views/landingPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/landingPage/header';
+
+import Demo from '../components/landingPage/demo';
+
+const LandingPage = function () {
+    return (
+       <>
+        <Header />
+        <Demo />
+        
+       </>
+    );
+}
+
+export default LandingPage;
